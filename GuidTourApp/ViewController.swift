6e84f126bb3b9b04a12208187f9b5bd9a23d42d0
Vu <vutq@itsol.vn>
2021-01-06 11:06:46 +0700

    Initial Commit

---
+++ GuidTourApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuidTourApp
+//
+//  Created by Vu on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
